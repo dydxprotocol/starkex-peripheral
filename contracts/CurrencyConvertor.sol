cc4c56ea7de88c5343392c8deb6afa93ad57a6de
Sam Weinberg <samweinberg23@gmail.com>
2021-08-16 15:22:41 -0400

    write contract

---
+++ contracts/CurrencyConvertor.sol
@@ -0,0 +1,32 @@
+/*
+    Copyright 2021 dYdX Trading Inc.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+pragma solidity ^0.8.7;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+contract Counter {
+  using SafeERC20 for IERC20;
+
+  // ============ Events ============
+
+  uint256 count = 0;
+  event CountedTo(uint256 number);
+
+  // ============ State-Changing Functions ============
+
+  function deposit(
+
+  )
+}
