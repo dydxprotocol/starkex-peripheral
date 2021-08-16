5daa78f0a4ce58fb38a200cb9b4045798d295c0e
Sam Weinberg <samweinberg23@gmail.com>
2021-08-16 14:42:07 -0400

    setup to run hardhat

---
+++ test/counter.ts
@@ -0,0 +1,50 @@
+import { ethers, waffle } from 'hardhat'
+import chai from "chai";
+import { solidity } from "ethereum-waffle";
+import CounterArtifact from '../artifacts/contracts/Counter.sol/Counter.json'
+import { Counter } from '../src/types/Counter'
+
+const { deployContract } = waffle
+
+
+chai.use(solidity);
+const { expect } = chai;
+describe("Counter", () => {
+  let counter: Counter;
+  beforeEach(async () => {
+    // 1
+    const signers = await ethers.getSigners();
+    // 2
+    const counterFactory = await ethers.getContractFactory(
+      "Counter",
+      signers[0]
+    );
+    counter = (await deployContract(signers[0], CounterArtifact)) as Counter
+    await counter.deployed();
+    const initialCount = await counter.getCount();
+    // 3
+    expect(initialCount).to.eq(0);
+    expect(counter.address).to.properAddress;
+  });
+  // 4
+  describe("count up", async () => {
+    it("should count up", async () => {
+      await counter.countUp();
+      let count = await counter.getCount();
+      expect(count).to.eq(1);
+    });
+  });
+  describe("count down", async () => {
+    // 5
+    it("should fail", async () => {
+      // this test will fail
+      await counter.countDown();
+    });
+    it("should count down", async () => {
+      await counter.countUp();
+    await counter.countDown();
+      const count = await counter.getCount();
+      expect(count).to.eq(0);
+    });
+  });
+});
